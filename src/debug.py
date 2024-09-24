4fa74e54c05a4623a788b8116c006586568a4d81
barbossini <tonga.sword@proton.me>
2024-09-24 21:26:06 +0200

    frames & canvas usage

--- src/debug.py
+++ src/debug.py
@@ -6,36 +6,33 @@ def start_interface():
     label = Label (mainWindow, text ="Debug window for testing new features",width = 50,bg = "white")
     label.pack()
 
+    # frame 1
+    Frame1 = Frame(mainWindow, borderwidth=2, relief=GROOVE)
+    Frame1.pack(side=LEFT, padx=30, pady=30)
 
-    # entrée
-    value = StringVar() 
-    value.set("texte par défaut")
-    entree = Entry(mainWindow, textvariable="value : ", width=30)
-    entree.pack()
+    # frame 2
+    Frame2 = Frame(mainWindow, borderwidth=2, relief=GROOVE)
+    Frame2.pack(side=LEFT, padx=10, pady=10)
 
-    # bouton de validation de donnée
-    boutonSend=Button(mainWindow, text="Send", command=mainWindow.update)
-    boutonSend.pack()
+    # frame 3 dans frame 2
+    Frame3 = Frame(Frame2, bg="white", borderwidth=2, relief=GROOVE)
+    Frame3.pack(side=RIGHT, padx=5, pady=5)
 
-    value = DoubleVar()
-    scale = Scale(mainWindow, variable=value)
-    scale.pack()
+    # Ajout de labels
+    Label(Frame1, text="Frame 1").pack(padx=10, pady=10)
+    Label(Frame2, text="Frame 2").pack(padx=10, pady=10)
+    Label(Frame3, text="Frame 3",bg="white").pack(padx=10, pady=10)
 
-    # liste
-    liste = Listbox(mainWindow)
-    liste.insert(1, "Python")
-    liste.insert(2, "PHP")
-    liste.insert(3, "jQuery")
-    liste.insert(4, "CSS")
-    liste.insert(5, "Javascript")
-    liste.pack()
+    def draw_circle(event):
+    # Dessiner un petit cercle autour du clic de la souris
+        x, y = event.x, event.y
+        canvas.create_oval(x-5, y-5, x+5, y+5, fill="blue")
 
-    # checkbutton
-    bouton = Checkbutton(mainWindow, text="Nouveau?")
-    bouton.pack()
+    canvas = Canvas(mainWindow, width=400, height=300, bg='white')
+    canvas.pack()
 
-    # bouton de sortie
-    boutonOutput=Button(mainWindow, text="Fermer", command=mainWindow.quit)
-    boutonOutput.pack()
-
+    # Liaison de l'événement de clic de souris au Canvas
+    canvas.bind("<Button-1>", draw_circle)
+        # Pour voir d'autres commandes ..
+    # print( dir(Canvas() ))
     mainWindow.mainloop()
